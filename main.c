24f5f61f2cb19315764ac984f736ba9c89efea2a
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-07 19:18:11 +0000

    Assigning 1-D array to a Pointer variable

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
  
 int main()
 {
-    int i = 12;
- 
-    printf("Address of i = %u \n", &i);
-    printf("Value of i = %d ", i);
- 
-    // signal to operating system program ran fine
-    return 0;
+  int my_arr[5] = {1, 2, 3, 4, 5}, i;
+  int *p;
+  p = my_arr;
+  // p = &my_arr[0]; // you can also do this
+
+  for(i = 0; i < 5; i++)
+  {
+      printf("Value of a[%d] = %d\t", i, *(p + i) );
+      printf("Address of a[%d] = %u\n", i, p + i );
+  }
+
+
+  // signal to operating system program ran fine
+  return 0;
 }
