75db29dae40e88254a386dee529efa0e02735a36
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-07 16:52:21 +0000

    Passing 1-D array elements to a function

--- main.c
+++ main.c
@@ -4,23 +4,31 @@
 
 #define SIZE 10
  
+void odd_or_even(int a);
+ 
 int main()
 {
-    int arr[5], i;
+    int my_arr[] = {13,56,71,38,93}, i;
  
     for(i = 0; i < 5; i++)
     {
-        printf("Enter a[%d]: ", i);
-        scanf("%d", &arr[i]);
-    } 
- 
-    printf("\nPrinting elements of the array: \n\n");
- 
-    for(i = 0; i < 5; i++)
-    {
-        printf("%d ", arr[i]);
+        // passing one element at a time to odd_or_even() function
+        odd_or_even(my_arr[i]); 
     }
  
     // signal to operating system program ran fine
     return 0;
+}
+ 
+void odd_or_even(int a)
+{
+    if(a % 2 == 0)
+    {
+        printf("%d is even\n", a);
+    }
+ 
+    else
+    {
+        printf("%d is odd\n", a);
+    }
 }
