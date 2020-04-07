1a7f7938a1ef7f25dd192d982f9609f4924bea26
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-07 17:57:53 +0000

    Address Operator (&)

--- main.c
+++ main.c
@@ -4,31 +4,13 @@
 
 #define SIZE 10
  
-void odd_or_even(int a);
- 
 int main()
 {
-    int my_arr[] = {13,56,71,38,93}, i;
+    int i = 12;
  
-    for(i = 0; i < 5; i++)
-    {
-        // passing one element at a time to odd_or_even() function
-        odd_or_even(my_arr[i]); 
-    }
+    printf("Address of i = %u \n", &i);
+    printf("Value of i = %d ", i);
  
     // signal to operating system program ran fine
     return 0;
-}
- 
-void odd_or_even(int a)
-{
-    if(a % 2 == 0)
-    {
-        printf("%d is even\n", a);
-    }
- 
-    else
-    {
-        printf("%d is odd\n", a);
-    }
 }
