ce000e391830e46e557fb41a52e29f73c17b772e
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-08 17:26:23 +0000

    Call by reference

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #define SIZE 10
  
-void try_to_change(int, int);
+void try_to_change(int *, int *);
  
 int main()
 {
@@ -15,22 +15,22 @@ int main()
  
     printf("\nCalling the function\n");
  
-    try_to_change(x, y);
+    try_to_change(&x, &y);
  
     printf("\nValues after function call\n\n");
  
     printf("Final value of x = %d\n", x);
     printf("Final value of y = %d\n", y);
  
-    // signal to operating system program ran fine
+    // signal to operating system everything works fine
     return 0;
 }
  
-void try_to_change(int x, int y)
+void try_to_change(int *x, int *y)
 {
-    x = x + 10;
-    y = y + 10;
+    (*x)++;
+    (*y)++;
  
-    printf("\nValue of x (inside function) = %d\n", x);
-    printf("Value of y (inside function) = %d\n", y);
+    printf("\nValue of x (inside function) = %d\n", *x);
+    printf("Value of y (inside function) = %d\n", *y);
 }
