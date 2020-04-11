d8750a140df8a7b92e5f6429572716b285f3e4ce
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-11 05:14:27 +0000

    Pointer Arithmetic in Void Pointers

--- main.c
+++ main.c
@@ -6,20 +6,14 @@
  
 int main()
 {
-    int i = 10;
-    float f = 2.34;
-    char ch = 'k';
+    int one_d[5] = {12, 19, 25, 34, 46}, i;
  
-    void *vptr;
+    void *vp = one_d;
  
-    vptr = &i;
-    printf("Value of i = %d\n", *(int *)vptr);
- 
-    vptr = &f;
-    printf("Value of f = %.2f\n", *(float *)vptr);
- 
-    vptr = &ch;
-    printf("Value of ch = %c\n", *(char *)vptr);
+    for(i = 0; i < 5; i++)
+    {
+        printf("one_d[%d] = %d\n", i, *( (int *)vp + i ) );
+    }
  
     // signal to operating system program ran fine
     return 0;
