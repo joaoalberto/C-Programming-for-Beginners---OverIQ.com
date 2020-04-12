d6809af5fa61599165ac3f77ded411e7832979bf
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-12 05:54:30 +0000

    program uses calloc() to create dynamic (it can vary in size at runtime) 1-D array.

--- main.c
+++ main.c
@@ -5,37 +5,32 @@
  
 int main()
 {
-    float *p, sum = 0;
-    int i, n;
+    int *p, i, n;
  
-    printf("Enter the number of students: ");
+    printf("Enter the size of the array: ");
     scanf("%d", &n);
  
-    // allocate memory to store n variables of type float
-    p = (float*)malloc(n*sizeof(float));
+    p = (int*)calloc(n, sizeof(int));
  
-    // if dynamic allocation failed exit the program
     if(p==NULL)
     {
         printf("Memory allocation failed");
         exit(1); // exit the program
     }
  
-    // ask the student to enter marks
     for(i = 0; i < n; i++)
     {
-        printf("Enter marks for %d student: ", i+1);
-        scanf("%f", p+i);
+        printf("Enter %d element: ", i);
+        scanf("%d", p+i);
     }
+    printf("\nprinting array of %d integers\n\n", n);
  
     // calculate sum
+ 
     for(i = 0; i < n; i++)
     {
-        sum += *(p+i);
+        printf("%d ", *(p+i));
     }
- 
-    printf("\nAverage marks = %.2f\n", sum/n);
- 
     // signal to operating system program ran fine
     return 0;
 }
