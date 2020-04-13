d8ba3c6e141ff899afb4bf10d67c8d55a6a19e7d
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-13 18:45:28 +0000

    program demonstrates how to use strcat() function

--- main.c
+++ main.c
@@ -6,29 +6,20 @@
  
 int main()
 {
-    char ch_arr1[20];
-    char ch_arr2[20];
+    char strg1[40];
+    char strg2[40];
  
-    printf("Enter first string (ch_arr_1): ");
-    fgets(ch_arr1, sizeof(ch_arr1), stdin);// gets(destination); -->> deprecated e substituted by fgets.
+    printf("Enter first string: ");
+    fgets(strg1, sizeof(strg1), stdin);// gets(destination); -->> deprecated e substituted by fgets.
  
-    printf("Enter second string(ch_arr_1): ");
-    fgets(ch_arr2, sizeof(ch_arr2), stdin);// gets(destination); -->> deprecated e substituted by fgets.
+    printf("Enter second string: ");
+    fgets(strg2, sizeof(strg2), stdin);// gets(destination); -->> deprecated e substituted by fgets.
  
-    printf("\nCopying first string into second... \n\n");
-    strcpy(ch_arr2, ch_arr1); // copy the contents of ch_arr1 to ch_arr2
+    printf("\nConcatenating first and second string .. \n\n");
+    strcat(strg1, strg2);
  
-    printf("First string (ch_arr_1) = %s\n", ch_arr1);
-    printf("Second string (ch_arr_2) = %s\n", ch_arr2);
- 
-    printf("\nCopying \"Greece\" string into ch_arr1 ... \n\n");
-    strcpy(ch_arr1, "Greece"); // copy Greece to ch_arr1
- 
-    printf("\nCopying \"Slovenia\" string into ch_arr2 ... \n\n");
-    strcpy(ch_arr2, "Slovenia"); // copy Slovenia to ch_arr2
- 
-    printf("First string (ch_arr_1) = %s\n", ch_arr1);
-    printf("Second string (ch_arr_2) = %s\n", ch_arr2);
+    printf("First string: %s\n", strg1);
+    printf("Second string: %s", strg2);
  
     // signal to operating system program ran fine
     return 0;
