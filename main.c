2e6b3ef2dd689603aef26df082a981664a1d48ab
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-13 03:47:52 +0000

    program calculates the length of the string entered by the user.

--- main.c
+++ main.c
@@ -2,17 +2,23 @@
 #include<math.h>
 #include "functions.h" // My local defined functions
 #include<stdlib.h>
+#include<string.h>
  
 int main()
 {
-    int i;
-    char str[6] = "hello";
-    char *p;
+    int len;
  
-    for(p = str; *p != '\0'; p++)
-    {
-        printf("Character = %c\t Address = %p\n", *(p), p);
-    }
+    // destination array can store only 30 characters including '\0'
+    char destination[30];
+ 
+    printf("Enter your dream destination: ");
+    // gets(destination); -->> deprecated e substituted by fgtes.
+    fgets(destination, sizeof(destination), stdin);
+ 
+    // calculate length of characters in destination
+    len = strlen(destination); 
+ 
+    printf("Your dream destination %s has %d characters in it", destination, len);
  
     // signal to operating system program ran fine
     return 0;
