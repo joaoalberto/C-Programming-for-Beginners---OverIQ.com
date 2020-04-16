7e214240675a69d6604d919dc4ac95c715f19618
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-16 14:36:11 +0000

    program asks the user to enter a username. If the username entered is one of the names in the master list then the user is allowed to calculate the factorial of a number.

--- main.c
+++ main.c
@@ -3,24 +3,48 @@
 #include "functions.h" // My local defined functions
 #include<stdlib.h>
 #include<string.h>
+int factorial(int );
  
 int main()
 {
-    int i;
+    int i, found = 0, n;
  
-    char ch_arr[3][10] = {
-                             "spike",
-                             "tom",
-                             "jerry"
-                         };
-    
-    printf("1st way \n\n");
+    char master_list[5][20] = {
+                                  "admin",
+                                  "tom",
+                                  "bob",
+                                  "tim",
+                                  "jim"
+                              }, name[10];
  
-    for(i = 0; i < 3; i++)
-    {
-        printf("string = %s \t address = %p\n", ch_arr + i, ch_arr + i);
+    printf("Enter username: ");
+    scanf("%s", &name);
+ 
+    for (i = 0; i < 5; i++) {
+      if (strcmp(name, master_list[i]) == 0) {
+            found = 1;
+            break;
+      }
+    }
+ 
+    if (found==1) {
+        printf("\nWelcome %s !\n", name);
+        printf("\nEnter a number to calculate the factorial: ");
+        scanf("%d", &n);
+        printf("Factorial of %d is %d", n, factorial(n)); }
+    else {
+        printf("Error: You are not allowed to run this program. %s", name);
     }
  
     // signal to operating system program ran fine
     return 0;
+}
+ 
+int factorial(int n) {
+    if(n == 0) {
+        return 1;
+    } 
+    else {
+        return n * factorial(n-1);
+    }
 }
