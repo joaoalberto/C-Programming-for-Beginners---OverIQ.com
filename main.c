87dae20a84ed93f554c3dcba31c8f032a4e02240
João Alberto Ferreira de Oliveira <joaoalberto@hotmail.com>
2020-04-25 02:55:04 +0000

    program demonstrates how to access strings literal in the array of pointers to string

--- main.c
+++ main.c
@@ -7,44 +7,22 @@ int factorial(int );
  
 int main()
 {
-    int i, found = 0, n;
+    int i = 1, *ip = &i;
  
-    char master_list[5][20] = {
-                                  "admin",
-                                  "tom",
-                                  "bob",
-                                  "tim",
-                                  "jim"
-                              }, name[10];
+    char *sports[] = {
+                         "golf",
+                         "hockey",
+                         "football",
+                         "cricket",
+                         "shooting"
+                     };
  
-    printf("Enter username: ");
-    scanf("%s", name);
- 
-    for (i = 0; i < 5; i++) {
-      if (strcmp(name, master_list[i]) == 0) {
-            found = 1;
-            break;
-      }
-    }
- 
-    if (found==1) {
-        printf("\nWelcome %s !\n", name);
-        printf("\nEnter a number to calculate the factorial: ");
-        scanf("%d", &n);
-        printf("Factorial of %d is %d\n\n", n, factorial(n)); }
-    else {
-        printf("Error: You are not allowed to run this program. %s\n\n", name);
+    for(i = 0; i < 5; i++)
+    {
+        printf("String = %10s", sports[i] );
+        printf("\tAddress of string literal = %p\n", sports[i]);
     }
  
     // signal to operating system program ran fine
     return 0;
-}
- 
-int factorial(int n) {
-    if(n == 0) {
-        return 1;
-    } 
-    else {
-        return n * factorial(n-1);
-    }
 }
